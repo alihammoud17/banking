568d85efd9936883d881564d55469c4136215e28
Ali Hammoud <alloochi.hammoud@gmail.com>
2024-08-11 20:21:21 +0300

    bank cards and routing

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,24 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
 
-    const loggedIn = {
-        firstName : "Ali"
+    const loggedIn: User = {
+        firstName: "Ali",
+        $id: '',
+        email: 'contact@ali.com',
+        userId: '',
+        dwollaCustomerUrl: '',
+        dwollaCustomerId: '',
+        lastName: '',
+        address1: '',
+        city: '',
+        state: '',
+        postalCode: '',
+        dateOfBirth: '',
+        ssn: ''
     }
 
   return (
@@ -25,7 +38,14 @@ const Home = () => {
                     totalCurrentBalance={1250.35}
                 />
             </header>
+
+            RECENT TRANSACTIONS
         </div>
+        <RightSidebar
+            user={loggedIn}
+            transactions={[]}
+            banks={[{currentBalance: 123.50}, {currentBalance: 155.50}]}
+        />
     </section>
   )
 }
