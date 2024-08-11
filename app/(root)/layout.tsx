b8f9e90899ffd60297f8345dd2844b2615090d00
Ali Hammoud <alloochi.hammoud@gmail.com>
2024-08-11 16:10:55 +0300

    Finalizing Sidebar with mobile view

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import MobileNav from '@/components/MobileNav';
+import Sidebar from '@/components/Sidebar';
+import Image from 'next/image';
 import React from "react";
 
 export default function RootLayout({
@@ -5,7 +8,36 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+    const loggedIn : User = {
+        firstName: 'Ali',
+        lastName: 'Hammoud',
+        $id: '',
+        email: '',
+        userId: '',
+        dwollaCustomerUrl: '',
+        dwollaCustomerId: '',
+        address1: '',
+        city: '',
+        state: '',
+        postalCode: '',
+        dateOfBirth: '',
+        ssn: ''
+    };
+
   return (
-    <main>SIDEBAR {children}</main>
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} /> 
+        
+        <div className="flex size-full flex-col">
+            <div className="root-layout">
+                <Image src='/icons/logo.svg' width={30} height={30} alt='menu icon' />
+                <div>
+                    <MobileNav user={loggedIn} />
+                </div>
+            </div>
+            {children}
+        </div>
+    </main>
   );
 }
